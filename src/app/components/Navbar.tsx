ea192c6891dacd3d76bccd4def912f2b2d09e354
Faizan Raza <faizanraza2221@gmail.com>
2024-10-31 00:40:32 +0500

    homepage nav and mobile menu setup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,38 @@
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div className="">Navbar</div>
-  )
-}
+    <div className="h-24 flex items-center justify-between">
+      {/* Left */}
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href={"/"} className="font-bold text-xl text-blue-600" >LAMASOCIAL</Link>
+      </div>
+      {/* Center */}
+      <div className="hidden md:flex w-[50%] text-sm">
+      {/* Links */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href='/' className="flex gap-2 items-center">
+            <Image src='/home.png' alt="Homepage" width={16} height={16} className="w-4 h-4" />
+            <span>Homepage</span>
+          </Link>
+          <Link href='/' className="flex gap-2 items-center">
+            <Image src='/friends.png' alt="Friendpage" width={16} height={16} className="w-4 h-4" />
+            <span>Friends</span>
+          </Link>
+          <Link href='/' className="flex gap-2 items-center">
+            <Image src='/stories.png' alt="Stories" width={16} height={16} className="w-4 h-4" />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <MobileMenu />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
